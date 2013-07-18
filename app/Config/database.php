6980e8fe0e6deeb2924c94980bcaca39231d96d7
k1LoW <k1lowxb@gmail.com>
2013-07-18 20:59:22 +0900

    Initial Commit
    CakePHP 2.3.8を設置
    app/tmp内にdb.sqlite3を設置。postsテーブルを作成
    bakeコマンドでPostのMVCを生成
    routes.phpを修正

---
+++ app/Config/database.php
@@ -0,0 +1,16 @@
+<?php
+class DATABASE_CONFIG {
+
+    public $default = array(
+        'datasource' => 'Database/Sqlite',
+        'persistent' => false,
+        'database' => DB_PATH,
+    );
+
+    public $test = array(
+        'datasource' => 'Database/Sqlite',
+        'persistent' => false,
+        'database' => TEST_DB_PATH,
+    );
+
+}
---
+++ app/Test/Fixture/PostFixture.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * PostFixture
+ *
+ */
+class PostFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'length' => 11, 'key' => 'primary'),
+		'title' => array('type' => 'text', 'null' => true, 'default' => null),
+		'body' => array('type' => 'text', 'null' => true, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			
+		),
+		'tableParameters' => array()
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'title' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'body' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'created' => '2013-07-18 20:54:05',
+			'modified' => '2013-07-18 20:54:05'
+		),
+	);
+
+}
